df1be816cbf13e107d2d14a6dff51ccc90b032f2
kingmakerdee <denzelndlovu63@gmail.com>
2023-02-14 17:44:18 -0800

    lists records with score >= 10 in desc order

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- lists all records with a score >= 10 in table second_table
+-- display score and name. sort by score in descending order
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
